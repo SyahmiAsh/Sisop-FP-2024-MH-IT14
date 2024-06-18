76d5873c3c556b8447e486cad5aaa552be2810a2
Muhammad Faqih Husain <88548292+fqhhusain@users.noreply.github.com>
2024-06-18 15:59:19 +0700

    Update discorit.c

--- fp/discorit.c
+++ fp/discorit.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
     char response[BUFFER_SIZE];
     char prompt[BUFFER_SIZE];
+    char current_channel[BUFFER_SIZE] = "";
 
     // Connect to the server
     connect_to_server(&client_socket, &server_addr);
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
     // Send initial command (REGISTER or LOGIN)
     snprintf(buffer, BUFFER_SIZE, "%s %s -p %s", argv[1], argv[2], argv[4]);
     send_command(client_socket, buffer, response);
-    printf("Server response: %s\n", response);
+    printf("%s\n", response);
 
     if (strstr(response, "berhasil login") != NULL) {
         snprintf(prompt, BUFFER_SIZE, "[%s] ", argv[2]);
@@ -83,33 +84,41 @@ int main(int argc, char *argv[]) {
                     send_command(client_socket, key_command, response);
                 }
                 // Update prompt
+                snprintf(current_channel, BUFFER_SIZE, "%s", channel_name);
                 snprintf(prompt, BUFFER_SIZE, "[%s/%s] ", argv[2], channel_name);
+                printf("%s\n%s", response, prompt);
+                continue;
             } else if (strncmp(buffer, "CREATE CHANNEL ", 15) == 0) {
                 // Send CREATE CHANNEL command
                 send_command(client_socket, buffer, response);
-                printf("Server response: %s\n", response);
+                printf("%s\n%s", response, prompt);
                 continue;
-            } else if (strncmp(buffer, "LIST", 4) == 0) {
-                // Send LIST command
+            } else if (strncmp(buffer, "LIST CHANNEL", 12) == 0) {
+                // Send LIST CHANNEL command
                 send_command(client_socket, buffer, response);
-                printf("Server response: %s\n", response);
+                printf("%s\n%s", response, prompt);
+                continue;
+            } else if (strncmp(buffer, "LIST USER", 9) == 0) {
+                // Send LIST USER command
+                send_command(client_socket, buffer, response);
+                printf("%s\n%s", response, prompt);
                 continue;
             } else if (strncmp(buffer, "EDIT PROFILE SELF -u ", 21) == 0) {
                 // Handle EDIT PROFILE SELF -u command
                 send_command(client_socket, buffer, response);
-                printf("Server response: %s\n", response);
+                printf("%s\n", response);
                 if (strstr(response, "Profil diupdate") != NULL) {
                     char *new_username = buffer + 21;
                     argv[2] = strdup(new_username); // Allocate new memory for the updated username
                     snprintf(prompt, BUFFER_SIZE, "[%s] ", new_username);
                 }
+                printf("%s", prompt); // Use updated prompt
                 continue;
             }
 
             // Send other commands
             send_command(client_socket, buffer, response);
-            printf("Server response: %s\n", response);
-            printf("%s", prompt); // Use updated prompt
+            printf("%s\n%s", response, prompt); // Print response and prompt
         }
     }
 
